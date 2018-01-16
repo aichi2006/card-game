6a04992c1d1980b482e697199e4bbe786e9a9d0f
shipengfei <136452103@qq.com>
2018-01-17 01:29:05 +0800

    commit

---
+++ src/js/index.js
@@ -0,0 +1,19 @@
+var app=new Vue({
+    el: '#box',
+    data: {
+        width:340,
+        height:340,
+        list: [
+            {left:0,top:0},
+            {left:70,top:0},
+            {left:140,top:0},
+            {left:210,top:0},
+            {left:280,top:0}
+        ]
+    },
+    methods: {
+        move: function(){
+            
+        }
+    }
+})
